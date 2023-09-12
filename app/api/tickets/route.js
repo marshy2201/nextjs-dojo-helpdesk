982cf09129cb06116a520be161d86ff64720771a
Lewis Marshall <marshy2201@yahoo.co.uk>
2023-09-12 15:19:38 +0100

    tickets POST endpoint added

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -9,3 +9,17 @@ export async function GET() {
 
  return NextResponse.json(tickets, { status: 200 });
 }
+
+export async function POST(response) {
+  const ticket = await response.json();
+
+  const res = await fetch('http://localhost:4000/tickets', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(ticket)
+  });
+
+  const newTicket = await res.json();
+
+  return NextResponse.json(newTicket, { status: 201 });
+}
