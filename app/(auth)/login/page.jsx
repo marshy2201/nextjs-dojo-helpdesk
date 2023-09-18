0c8bbd65eaa582bc568e30aa4709d41c816bd100
Lewis Marshall <marshy2201@yahoo.co.uk>
2023-09-18 15:24:56 +0100

    authentication added

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,7 +1,45 @@
+"use client"
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+// components
+import AuthForm from "../AuthForm";
+
 export default function Login() {
+  const router = useRouter();
+  const [formError, setFormError] = useState('');
+
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+
+    setFormError('');
+
+    const supabase = createClientComponentClient();
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password
+    });
+  
+    if (!error) {
+      router.push('/');
+    }
+
+    if (error) {
+      setFormError(error.message);
+    }
+  } 
+
   return (
     <main>
       <h2 className="text-center">Log In</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
+
+      {formError && (
+        <div className="error">{formError}</div>
+      )}
     </main>
   )
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,7 +1,45 @@
+"use client"
+
+import { useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+
+// components
+import AuthForm from "../AuthForm";
+
 export default function Signup() {
+  const router = useRouter();
+  const [formError, setFormError] = useState('')
+
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+  
+    const supabase = createClientComponentClient();
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback`
+      }
+    });
+  
+    if (error) {
+      setFormError(error.message);
+    }
+  
+    if (!error) {
+      router.push('/verify');
+    }
+  }
+
   return (
     <main>
       <h2 className="text-center">Sign Up</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
+      {formError && (
+        <div className="error">{formError}</div>
+      )}
     </main>
   )
 }
